75e718bb2569a86b1dcea77a770b6de4b36f295d
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-08-29 17:31:57 -0300

    app

--- app/src/main/java/br/com/espe/controlxfood/Views/View_Login.java
+++ app/src/main/java/br/com/espe/controlxfood/Views/View_Login.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
 import br.com.espe.controlxfood.R;
 
 public class View_Login extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView imagem_configuracoes;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_login);
+
+        imagem_configuracoes = findViewById(R.id.imagem_configuracoes);
+        imagem_configuracoes.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.imagem_configuracoes:
+                break;
+        }
     }
 }
