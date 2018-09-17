6af4c7098aba5513f1060b23ba62f41cef8f006d
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-09-17 12:21:17 -0300

    aoo

--- app/src/main/java/br/com/espe/controlxfood/Views/View_Login.java
+++ app/src/main/java/br/com/espe/controlxfood/Views/View_Login.java
@@ -1,30 +1,62 @@
 package br.com.espe.controlxfood.Views;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import br.com.espe.controlxfood.R;
 
 public class View_Login extends AppCompatActivity implements View.OnClickListener {
-
-    ImageView imagem_configuracoes;
-
+    
+    Toolbar toolbar;
+    Button button_login;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_login);
+        createToolbar(toolbar);
 
-        imagem_configuracoes = findViewById(R.id.imagem_configuracoes);
-        imagem_configuracoes.setOnClickListener(this);
+        button_login = findViewById(R.id.button_login);
+        button_login.setOnClickListener(this);
+    }
+
+    private void createToolbar(Toolbar toolbar) {
+        toolbar = findViewById(R.id.toolbar_login);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.configuracoes_login:
+
+                break;
+        }
+        return true;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.imagem_configuracoes:
+            case R.id.button_login:
+                startActivity(new Intent(this, View_Venda.class));
+                finish();
                 break;
         }
     }
---
+++ app/src/main/java/br/com/espe/controlxfood/Views/View_Venda.java
@@ -0,0 +1,22 @@
+package br.com.espe.controlxfood.Views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import br.com.espe.controlxfood.R;
+
+public class View_Venda extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_venda);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
