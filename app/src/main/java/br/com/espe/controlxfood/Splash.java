5de8b6b48643b8a6f433355cbaa31f3cd562506f
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-09-19 09:41:19 -0300

    app

--- app/src/main/java/br/com/espe/controlxfood/Splash.java
+++ app/src/main/java/br/com/espe/controlxfood/Splash.java
@@ -21,43 +21,26 @@ import br.com.espe.controlxfood.Views.View_Login;
 public class Splash extends AppCompatActivity {
 
     int count;
-    ImageView imagelogo;
     ProgressBar progress;
-    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_splash);
         count = 0;
-        imagelogo = findViewById(R.id.imagelogo);
         progress = findViewById(R.id.progress);
-        progress.getProgressDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
-        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_rotate);
-        imagelogo.startAnimation(animation);
+        progress.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
         new CountDownTimer(5000, 25){
             @Override
             public void onTick(long millisUntilFinished) {
                 count++;
                 progress.setProgress(count);
             }
-
             @Override
             public void onFinish() {
-                imagelogo.clearAnimation();
                 startActivity(new Intent(getApplicationContext(), Intro.class));
                 finish();
             }
         }.start();
-
-
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                startActivity(new Intent(Splash.this, View_Login.class));
-//                finish();
-//            }
-//        }, 5000);
-
     }
 }
